6b70354c5c91144d18c48048a7780bcb8f6378e8
atrelinski <atrel.devel1@gmail.com>
2018-12-30 12:45:30 +0100

    Shortening file names

--- PBMWindow.cs
+++ PBMWindow.cs
@@ -16,19 +16,19 @@
     /// </para>
     /// </remarks>
     [Guid("55ff594a-8ecc-4b14-9c9a-45f869673d62")]
-    public class ParallelBuildsMonitorWindow : ToolWindowPane
+    public class PBMWindow : ToolWindowPane
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="ParallelBuildsMonitorWindow"/> class.
+        /// Initializes a new instance of the <see cref="PBMWindow"/> class.
         /// </summary>
-        public ParallelBuildsMonitorWindow() : base(null)
+        public PBMWindow() : base(null)
         {
             this.Caption = "Parallel Builds Monitor";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
             // the object returned by the Content property.
-            this.Content = new ParallelBuildsMonitorWindowControl();
+            this.Content = new PBMControl();
         }
 
     }
