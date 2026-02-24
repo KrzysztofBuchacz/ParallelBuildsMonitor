40a9111029e482a21a091759f1a9e43568e50486
atrelinski <atrelinski@no-mail.com>
2026-02-24 22:50:42 +0100

    Provide MachineInfoIndependentScope and update test images

--- Plugin/MachineInfo.cs
+++ Plugin/MachineInfo.cs
@@ -131,7 +131,7 @@ namespace ParallelBuildsMonitor
 
         public override string ToString()
         {
-            return ToString("  |  ");
+            return ToString(" | ");
         }
 
         public string ToString(string separator)
